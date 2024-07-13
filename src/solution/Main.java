bad8fe1867b60b30ddedbac827d2d42afc004562
Austtiin <austinstephens103@gmail.com>
2024-07-13 10:19:58 -0500

    reupload

---
+++ src/solution/Main.java
@@ -0,0 +1,88 @@
+/*
+ * main.java
+ * Class to run the solution
+ * Solution will be separated into multiple classes for better organization
+ *
+ * Created by: Austin Stephens
+ * Created on: 07/10/2024
+ * Rasumssen University
+ * Professor: Kumar
+ *
+ * Instructions: To handle the modules in your operating system, you will need to implement a stack.
+ * The stack will be for holding the modules in the order in which they have to be compiled.
+ * You may use any data structure, singly-linked list, etc. You may NOT use the Java Stack Class.
+ * However, your program must be able to handle the three stack functions; push, pop, and peek.
+ *
+ * The final program should be able to take a list of data items and generate a stack.
+ * You are free to specify the data type of the items.
+ * However, keep in mind that these items will represent your operating system modules
+ * You can choose to have the data type as strings
+ * (Module A, Module B, etc. …), characters (A, B, C…) or integers (1, 2,3…).
+ *
+ */
+
+package solution;
+import java.util.Scanner;
+
+
+
+public class main {
+
+    moduleFunc func = new moduleFunc();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        main main = new main();
+        main.run();
+
+        public void run() {
+            System.out.println("MOdule -02 Assignment Stack Implementation");
+            System.out.println("Please enter Module names to be added to the stack (A, B, C…)");
+            System.out.println("Press Enter after each module name");
+            System.out.println("Enter nothing to stop adding modules");
+            moduleLearn();
+            func.print();
+
+
+
+
+            System.out.println("Modules have been added to the stack");
+            System.out.println("Please select an option");
+            System.out.println("1. Pop");
+            System.out.println("2. Peek");
+            System.out.println("3. Print");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    func.pop();
+                    break;
+                case 2:
+                    func.peek();
+                    break;
+                case 3:
+                    func.print();
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+
+        private void moduleLearn() {
+            int x = 0;
+            while (x != 1) {
+                System.out.println("Enter module name: ");
+                String module = scanner.nextLine();
+
+                if (!module.isEmpty()) {
+                    func.push(module);
+                    module = scanner.nextLine();
+
+                } else {
+                    x = 1;
+                    System.out.println("No modules added, moving on...");
+                }
+            }
+
+        }
+
+    }
---
+++ src/solution/moduleFunc.java
@@ -0,0 +1,69 @@
+/*
+ * moduleFunc.java
+ * Class to handle the stack functions
+ *
+ * Created by: Austin Stephens
+ * Created on: 07/10/2024
+ * Rasumssen University
+ * Professor: Kumar
+ *
+ */
+
+
+package solution;
+
+import java.util.ArrayList;
+
+public class moduleFunc {
+    ArrayList<String> stack = new ArrayList<String>();
+
+
+    public void push(String module) {
+        // Push function
+        // Add module to stack
+        // Check if module name is empty
+        try {
+            if (module.isEmpty()) {
+                System.out.println("Module name cannot be empty");
+                return;
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Module name cannot be empty");
+            return;
+        }
+        // Add module to stack
+        stack.add(module);
+        System.out.println("Module added.");
+    }
+
+    // Pop function
+    public void pop() {
+        if (stack.size() == 0) {
+            System.out.println("Stack is empty");
+        } else {
+            stack.remove(stack.size() - 1);
+        }
+    }
+
+    // Peek function
+    public void peek() {
+        if (stack.size() == 0) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.println(stack.get(stack.size() - 1));
+        }
+    }
+
+    // Print function
+    public void print() {
+        System.out.println("Modules in the stack are: ");
+        if (stack.size() == 0) {
+            System.out.println("Our Stack is empty");
+        } else {
+            for (int i = 0; i < stack.size(); i++) {
+                System.out.println(stack.get(i));
+            }
+        }
+    }
+
+}
